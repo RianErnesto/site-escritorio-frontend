da5d1372c1d9997a69bd5e015dc3fdc1c1f58f3e
RianErnesto <rianernesto9@gmail.com>
2022-03-21 22:54:13 -0300

    Parte de bootstrap do cabeçalho feito porém não adaptado

--- src/App.js
+++ src/App.js
@@ -4,20 +4,47 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+      <header>
+        <nav className='navbar navbar-expand-lg navbar-dark bg-dark' aria-label='Tenth navbar example'>
+          <div className='container-fluid'>
+            <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarsExample08' 
+              aria-controls='navbarsExample08' aria-expanded='false' aria-label='Toggle navigation'>
+                <span className='navbar-toggler-icon' />
+            </button>
+            <div className='collapse navbar-collapse justify-content-md-center' id='navbarsExample08'>
+              <ul className='navbar-nav'>
+                <li className='nav-item'>
+                  <a className='nav-link active' aria-current='page' href='#'>Centered nav only</a>
+                </li>
+                <li className='nav-item'>
+                  <a className='nav-link' href='#'>Link</a>
+                </li>
+                <li className='nav-item'>
+                  <a className='nav-link disabled'>Disabled</a>
+                </li>
+                <li className='nav-item dropdown'>
+                  <a className='nav-link dropdown-toggle' href='#' id='dropdown08' data-bs-toggle='dropdown' aria-expanded='false'>
+                    Dropdown
+                  </a>
+                  <ul className='dropdown-menu' aria-labelledby='dropdown08'>
+                    <li>
+                      <a className='dropdown-item' href='#'>Action</a>
+                    </li>
+                    <li>
+                      <a className='dropdown-item' href='#'>Another action</a>
+                    </li>
+                    <li>
+                      <a className='dropdown-item' href='#'>Something else here</a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
       </header>
+
     </div>
   );
 }
