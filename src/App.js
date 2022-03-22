3efbe79718988731fef2d7b969966dd1afa2f2f9
RianErnesto <rianernesto9@gmail.com>
2022-03-21 23:02:55 -0300

    Cabeçalho Bootstrap feito e adaptado para o site

--- src/App.js
+++ src/App.js
@@ -6,38 +6,32 @@ function App() {
     <div className="App">
 
       <header>
-        <nav className='navbar navbar-expand-lg navbar-dark bg-dark' aria-label='Tenth navbar example'>
+        <nav className='navbar navbar-expand-lg navbar-dark bg-dark' aria-label='NavBar Site'>
           <div className='container-fluid'>
-            <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarsExample08' 
-              aria-controls='navbarsExample08' aria-expanded='false' aria-label='Toggle navigation'>
+            <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarSite' 
+              aria-controls='navbarSite' aria-expanded='false' aria-label='Toggle navigation'>
                 <span className='navbar-toggler-icon' />
             </button>
-            <div className='collapse navbar-collapse justify-content-md-center' id='navbarsExample08'>
+            <div className='collapse navbar-collapse justify-content-md-center' id='navbarSite'>
+              {/* Link que contém a imagem */}
+              <a className="navbar-brand" href="#">
+                <img src="/docs/5.1/assets/brand/bootstrap-logo.svg" alt="" width="30" height="24" />
+              </a>
               <ul className='navbar-nav'>
                 <li className='nav-item'>
-                  <a className='nav-link active' aria-current='page' href='#'>Centered nav only</a>
+                  <a className='nav-link active' aria-current='page' href='#'>Home</a>
                 </li>
                 <li className='nav-item'>
-                  <a className='nav-link' href='#'>Link</a>
+                  <a className='nav-link' href='#'>Sobre</a>
                 </li>
                 <li className='nav-item'>
-                  <a className='nav-link disabled'>Disabled</a>
+                  <a className='nav-link' href='#'>Destaques</a>
                 </li>
-                <li className='nav-item dropdown'>
-                  <a className='nav-link dropdown-toggle' href='#' id='dropdown08' data-bs-toggle='dropdown' aria-expanded='false'>
-                    Dropdown
-                  </a>
-                  <ul className='dropdown-menu' aria-labelledby='dropdown08'>
-                    <li>
-                      <a className='dropdown-item' href='#'>Action</a>
-                    </li>
-                    <li>
-                      <a className='dropdown-item' href='#'>Another action</a>
-                    </li>
-                    <li>
-                      <a className='dropdown-item' href='#'>Something else here</a>
-                    </li>
-                  </ul>
+                <li className='nav-item'>
+                  <a className='nav-link' href='#'>Projetos</a>
+                </li>
+                <li className='nav-item'>
+                  <a className='nav-link' href='#'>Contato</a>
                 </li>
               </ul>
             </div>
