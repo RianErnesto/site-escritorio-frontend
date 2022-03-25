af7489d73e7e8fda9faefa9055f39cd8eec23b40
rianernesto9@gmail.com <rianernesto9@gmail.com>
2022-03-25 15:15:42 -0300

    Header adicionado e quase finalizado, criados os componentes NavBar e Header

--- site-escritorio/src/App.jsx
+++ site-escritorio/src/App.jsx
@@ -1,39 +1,10 @@
-import * as React from 'react';
-import Breadcrumbs from '@mui/material/Breadcrumbs';
-import Typography from '@mui/material/Typography';
-import Link from '@mui/material/Link';
-import Stack from '@mui/material/Stack';
-import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+import React from 'react';
+import HomeAppBar from './Header';
 
-function handleClick(event) {
-  event.preventDefault();
-  console.info('You clicked a breadcrumb.');
-}
-
-export default function CustomSeparator() {
-  const breadcrumbs = [
-    <Link underline="hover" key="1" color="inherit" href="/" onClick={handleClick}>
-      Home
-    </Link>,
-    <Link underline="hover" key="2" color="inherit" href="/getting-started/installation/" onClick={handleClick}>
-      Sobre
-    </Link>,
-    <Link underline="hover" key="2" color="inherit" href="/getting-started/installation/" onClick={handleClick}>
-      Destaques
-    </Link>,
-    <Link underline='hover' key="3" color="text.primary" href='/' onClick={handleClick}>
-      Projetos
-    </Link>,
-    <Link underline='hover' key="3" color="text.primary" href='/' onClick={handleClick}>
-      Membros
-    </Link>
-  ];
-
+function App() {
   return (
-    <Stack spacing={2}>
-      <Breadcrumbs separator="|" aria-label="breadcrumb">
-        {breadcrumbs}
-      </Breadcrumbs>
-    </Stack>
+    <HomeAppBar />
   );
 }
+
+export default App;
