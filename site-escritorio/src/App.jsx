5f3c17b4aebb80059e9c1e4d1cb4504f300f190c
RianErnesto <rianernesto9@gmail.com>
2022-03-25 11:37:23 -0300

    Menu do header criado

--- site-escritorio/src/App.jsx
+++ site-escritorio/src/App.jsx
@@ -1,7 +1,50 @@
-function App() {
-  return(
-    <h1>Site Escritório de Engenharia</h1>
-  );
+// function App() {
+//   return(
+//     <h1>Site Escritório de Engenharia</h1>
+//   );
+// }
+
+// export default App;
+
+
+
+
+import * as React from 'react';
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+import Stack from '@mui/material/Stack';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+
+function handleClick(event) {
+  event.preventDefault();
+  console.info('You clicked a breadcrumb.');
 }
 
-export default App;
+export default function CustomSeparator() {
+  const breadcrumbs = [
+    <Link underline="hover" key="1" color="inherit" href="/" onClick={handleClick}>
+      Home
+    </Link>,
+    <Link underline="hover" key="2" color="inherit" href="/getting-started/installation/" onClick={handleClick}>
+      Sobre
+    </Link>,
+    <Link underline="hover" key="2" color="inherit" href="/getting-started/installation/" onClick={handleClick}>
+      Destaques
+    </Link>,
+    <Link underline='hover' key="3" color="text.primary" href='/' onClick={handleClick}>
+      Projetos
+    </Link>,
+    <Link underline='hover' key="3" color="text.primary" href='/' onClick={handleClick}>
+      Membros
+    </Link>
+  ];
+
+  return (
+    <Stack spacing={2}>
+      <Breadcrumbs separator="|" aria-label="breadcrumb">
+        {breadcrumbs}
+      </Breadcrumbs>
+    </Stack>
+  );
+}
