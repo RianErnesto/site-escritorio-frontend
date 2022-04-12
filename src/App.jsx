a418dcbf9e17c7fd8274a104af0ed98b85c889fc
rianernesto9@gmail.com <rianernesto9@gmail.com>
2022-04-12 13:46:49 -0300

    Criada a pasta da pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,10 @@
 import React from "react";
-import Home from "./Components/Home/Home";
-import Grupo from "./Components/Grupos/Grupo";
-import Projetos from "./Components/Projetos/Projetos";
-import Sobre from "./Components/Sobre/Sobre";
-import Membros from "./Components/Membros/Membros";
-import Destaques from "./Components/Destaques/Destaques";
-import Contatos from './Components/Contatos/Contatos';
+import Main from "./Pages/Main/Main";
 import ReactDOM from "react-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Main />
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-function App() {
-  return (
-    <div>
-      <div id='Home'>
-        <Home />
-      </div>
-      <div id='Grupo'>
-        <Grupo />
-      </div>
-      <div id='Projetos'>
-        <Projetos />
-      </div>
-      <div id='Sobre'>
-        <Sobre />
-      </div>
-      <div id='Membros'>
-        <Membros />
-      </div>
-      <div id='Destaques'>
-        <Destaques />
-      </div>
-      <div id='Contatos'>
-        <Contatos />
-      </div>
-    </div>
-  );
-}
---
+++ src/Pages/Projeto/Projeto.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Projeto() {
+    return(
+        <h1>Alo</h1>
+    );
+}
+
+export default Projeto;
