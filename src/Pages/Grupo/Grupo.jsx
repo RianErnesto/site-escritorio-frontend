79f4717abbd3155e33a26d19f4c3ea50c4f6fb0c
rianernesto9@gmail.com <rianernesto9@gmail.com>
2022-06-30 15:30:31 -0300

    Versão 1.0 atualizada

--- src/Pages/Grupo/Grupo.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Grupo() {
-    return(
-        <h1>Ola</h1>
-    );
-}
-
-export default Grupo;
--- src/Pages/Projeto/Projeto.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Projeto() {
-    return(
-        <h1>Alo</h1>
-    );
-}
-
-export default Projeto;
